7f1f49cda75c74e57d8d1624d37006593050fc1b
MaxPsm <43060782+MaxPsm@users.noreply.github.com>
2021-10-25 16:52:32 +0300

    Create 8_Mongo.sql

---
+++ 8_Mongo.sql
@@ -0,0 +1,7 @@
+db.getCollection('restaurants').find(
+                            {
+                             "cuisine" : {$ne : "American"},
+                             "grades.score" :{$gt: 70},
+                             "address.coord.0" : {$lt : -65.754168}
+                            }
+                        )
