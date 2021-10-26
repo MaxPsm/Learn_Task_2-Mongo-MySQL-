9ec744df6445295bf44ad1ca08cccaf67f5eb7ed
MaxPsm <43060782+MaxPsm@users.noreply.github.com>
2021-10-26 12:37:54 +0300

    Update 15_Mongo.js

--- MongoDB/15_Mongo.js
+++ MongoDB/15_Mongo.js
@@ -1,1 +1,2 @@
-db.getCollection('restaurants').find({"grades.score" : { $not: {$gt : 10}}})
+db.getCollection('restaurants').find({"grades.score" : { $not: {$gt : 10}}},
+                                    {restaurant_id: 1, name: 1, borough: 1, cuisine: 1})
