105cbd9aa7801e6da64904e87c8e4cba231ae2c2
MaxPsm <43060782+MaxPsm@users.noreply.github.com>
2021-10-27 16:24:35 +0300

    Create 3.sql

---
+++ MySQL/3.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (email, pass, sum, created)
+VALUES
+("msk@yandex.ru", MD5("user1"), 20, '2008-01-01 00:00:01'),
+("rew@mail.ru", MD5("user3"), 100, '2020-05-24'),
+("gls@mail.ru", MD5("user4"), 10, '2020-10-30'),
+("wer@gmail.ru", MD5("user5"), 70, '2021-10-27'),
+("genry@gmail.ru", MD5("user6"), 170, '2021-10-27'),
+("limp@gmail.ru", MD5("user7"), 30, '2021-10-27');
